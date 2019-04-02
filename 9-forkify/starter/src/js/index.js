8a6e890c95cf8cd95505fda9cb2cf62ecbf1c9eb
Xiaomei Wang <xiaomei.wang07@gmail.com>
2019-04-02 11:47:46 +0800

    finish course

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,1 +1,9 @@
 // Global app controller
+//import str from './models/Search'
+
+//import { add as a, multiply as m, ID } from './views/searchView';
+//import * as searchView from './views/searchView';
+
+//console.log(`Using imported functions! ${a(ID, 2)} and ${m(3, 5)}. ${str}`);
+//console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}. ${str}`);
+
